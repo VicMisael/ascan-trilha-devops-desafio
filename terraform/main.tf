a7681a68202e7419ccc1368c11ed9b76b2ca52ce
Victor Misael <misael1805@gmail.com>
2022-11-29 22:43:22 -0300

    fix task definitions

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,44 @@ resource "aws_ecs_cluster" "ascan_devops_cluster" {
   name = "ascan_devops_cluster" # Naming the cluster
 }
 
+resource "aws_security_group" "web-node" {
+  vpc_id = aws_default_vpc.default_vpc.id
+  name = "Security group"
+  description = "Web Security Group"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_ecs_service" "ascan_devops_service" {
+  name            = "ascan-devops-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.ascan_devops_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.ascan_devops.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 1 # Setting the number of containers we want deployed to 3
+    network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}"]
+    security_groups = [ "${aws_security_group.web-node.id}" ]
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
+
 resource "aws_ecs_task_definition" "ascan_devops" {
   family                   = "ascan_devops" # Naming our first task
   container_definitions    = <<DEFINITION
@@ -37,8 +75,18 @@ resource "aws_ecs_task_definition" "ascan_devops" {
         }
       ],
       "memory": 512,
-      "cpu": 256
+      "cpu": 256,
+        "logConfiguration": {
+        "logDriver": "awslogs",
+              "options": {
+                  "awslogs-group": "firelens-container",
+                  "awslogs-region": "us-east-1",
+                  "awslogs-create-group": "true",
+                  "awslogs-stream-prefix": "firelens"
+                }   
+        }
     }
+
   ]
   DEFINITION
   requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
@@ -46,6 +94,13 @@ resource "aws_ecs_task_definition" "ascan_devops" {
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
