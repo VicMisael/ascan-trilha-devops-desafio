2898feffd0912288d1c920f084625c71de21b421
Victor Misael <misael1805@gmail.com>
2022-11-29 23:24:32 -0300

    new task definition and load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,12 +52,66 @@ resource "aws_ecs_service" "ascan_devops_service" {
   cluster         = "${aws_ecs_cluster.ascan_devops_cluster.id}"             # Referencing our created Cluster
   task_definition = "${aws_ecs_task_definition.ascan_devops.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 1 # Setting the number of containers we want deployed to 3
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
     network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}"]
-    security_groups = [ "${aws_security_group.web-node.id}" ]
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    //security_groups = [ "${aws_security_group.web-node.id}" ]
+    security_groups  = ["${aws_security_group.service_security_group.id}"]
     assign_public_ip = true # Providing our containers with public IPs
   }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.web-node.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path = "/alive"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
+
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.web-node.id}"]
 }
 
 resource "aws_ecs_task_definition" "ascan_devops" {
@@ -101,6 +155,12 @@ resource "aws_default_vpc" "default_vpc" {
 }
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "us-east-1a"
+}
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1c"
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
