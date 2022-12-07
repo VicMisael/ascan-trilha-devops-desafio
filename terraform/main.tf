9bdee83144c645d885c993b1171c5fc980649598
Victor Misael <misael1805@gmail.com>
2022-12-07 12:16:53 -0300

    fix logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,12 +134,14 @@ resource "aws_ecs_task_definition" "ascan_devops" {
           "hostPort": 8080
         }
       ],
-      "logDriver": "awslogs",
-      "options": {
-            "awslogs-group": "${aws_cloudwatch_log_group.aws_ecs_log_group.name}",
-            "awslogs-region": "us-east-1",
-            "awslogs-stream-prefix": "ecs"
-          },
+      "logConfiguration":{
+        "logDriver": "awslogs",
+        "options": {
+              "awslogs-group": "${aws_cloudwatch_log_group.aws_ecs_log_group.name}",
+              "awslogs-region": "us-east-1",
+              "awslogs-stream-prefix": "ecs"
+            }
+      },
       "memory": 512,
       "cpu": 256
     }
